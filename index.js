defa73096398adeafcbd86a71a6f0eb1c6ee2424
SubArrest <d.wiggin1613@gmail.com>
2020-11-12 14:50:28 +0000

    Actually Made The API Work

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-const ytdl = require("youtube-mp3-downloader");
 const fs = require("fs");
+const ytdl = require("ytdl-core");
 const YouTube = require("simple-youtube-api")
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath("/usr/bin/ffmpeg");
+const { config } = require("dotenv");
+config({
+	path: "/root/apis/ytdl/.env"
+});
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 const token = process.env.TOKEN;
 
+const youtube = new YouTube(token);
+
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
 app.get("/ytdl", (req, res) => {
-    const youtube = new YouTube(token);
-    const YD = new ytdl({
-        "ffmpegPath": "/usr/bin/ffmpeg",
-        "outputPath": "/root/apis/ytdl/downloads",
-        "youtubeVideoQuality": "highest",
-    });
-    function youtube_parser(url){
-        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-        var match = url.match(regExp);
-        return (match&&match[7].length==11)? match[7] : false;
-    }
     const ytURL = req.query.link
     if(!ytURL) return res.send({
         error: 400,
-        message: "Not A Valid Youtube Video URL Or Video ID"
+        message: "Not A Valid Youtube Video URL Or Video ID [1]"
     });
     fs.access("/root/apis/ytdl/downloads/"+youtube_parser(ytURL)+".json", fs.F_OK, function(err){
 		if (err) {
 			if(youtube_parser(ytURL) !== false){
 				youtube.getVideo(ytURL)
-                .then(video => {
+                .then(async video => {
                     const Duration = video.durationSeconds
                     if(Duration>600){
                         return res.send({
@@ -41,50 +44,60 @@ app.get("/ytdl", (req, res) => {
                         });
                     }
                     else{
-                        YD.download(youtube_parser(ytURL));
-                        YD.on("finished", (err, data) => {
-                            if(err){
+                        const ytID = youtube_parser(ytURL);
+                        let info = await ytdl.getInfo(ytURL);
+                        const stream = ytdl.downloadFromInfo(info,{
+                            quality: 'highestaudio', 
+                            filter: format => format.container === 'mp4' && format.hasAudio && format.hasVideo
+                        });
+                        stream.on("response", response => {
+                            new ffmpeg({
+                                source: stream
+                            })
+                            .toFormat("mp3")
+                            .on("end", () => {
+                                youtube.getVideo(ytURL).then(video => {
+                                    const edit = {
+                                        videoId: video.id,
+                                        videoTitle: video.title,
+                                        thumbnail: video.thumbnails.high.url,
+                                        url: `https://pi.substuff.org/api/ytdl/downloads/${ytID}.mp3`,
+                                        channel: video.channel.title
+                                    };
+                                    const jsonStr = JSON.stringify(edit);
+                                    fs.writeFileSync(`/root/apis/ytdl/downloads/${ytID}.json`, jsonStr);
+                                    return res.send(edit);
+                                });
+                            })
+                            .on("error", err => {
+                                console.error(err);
                                 return res.send({
                                     error: 500,
                                     message: "Error while trying to get video [1]"
                                 });
-                            }
-                            else{
-                                const vidID = data.videoId;
-                                let edit = data;
-                                edit.url = `http://vs.substuff.org:${port}/ytdl/downloads/${vidID}.mp3`;
-                                fs.renameSync(edit.file,`/root/apis/ytdl/downloads/${vidID}.mp3`);
-                                delete edit.file;
-                                delete edit.artist;
-                                delete edit.youtubeUrl;
-                                delete edit.stats;
-                                delete edit.title;
-                                youtube.getVideo(ytURL).then(video => {
-                                    edit.channel = video.channel.title;
-                                    const jsonStr = JSON.stringify(edit);
-                                    fs.writeFileSync(`/root/apis/ytdl/downloads/${vidID}.json`, jsonStr);
-                                    return res.send(edit);
-                                });
-                            }
+                            })
+                            .pipe(fs.createWriteStream(`/root/apis/ytdl/downloads/${ytID}.mp3`));
                         });
-                        YD.on("error", err => {
+                        stream.on("error", err => {
+                            console.error(err);
                             return res.send({
                                 error: 500,
-                                message: "Error while trying to get video [1]"
+                                message: "Error while trying to get video [2]"
                             });
                         });
                     }
                 })
-                .catch(() => {
+                .catch(err => {
+                    console.error(err);
                     return res.send({
                         error: 400,
-                        message: "Not A Valid Youtube Video URL Or Video ID"
+                        message: "Not A Valid Youtube Video URL Or Video ID [2]"
                     });
                 });
 			}
 			else return res.send({
                 error: 400,
-                message: "Not A Valid Youtube Video URL Or Video ID"
+                message: "Not A Valid Youtube Video URL Or Video ID [3]"
             });
 		}
 		else{
